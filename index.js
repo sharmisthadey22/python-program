43b19d74182a8ddd1846934b73c75280a828bbf9
sharmisthadey22 <sharmisthamayur@gmail.com>
2026-01-07 21:54:20 +0530

    hye hi

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require ('express');
+const fs = require('fs');
 const app = express ();
 
 app.use (express.json ());
@@ -8,6 +9,11 @@ app.set ('view engine', 'ejs');
 
 app.get ('/', function (req, res) {
    res.render ("index");
+});
+
+fs.writeFile("hey.txt ", "hey hello kaise ho", function(err) {
+    if(err) console.error(err);
+    else console.log("done");
 });
 
 
