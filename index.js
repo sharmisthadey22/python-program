dd0bb761b6cc537bfd2aa77c9667fc1f48507d4c
sharmisthadey22 <sharmisthamayur@gmail.com>
2026-01-09 00:26:35 +0530

    tf

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require ('express');
-const fs = require('fs');
+const path = require ('path');
+//const fs = require();
 const app = express ();
 
 app.use (express.json ());
 app.use (express.urlencoded ({ extended: true }));
+app.use (express.static (path.join (__dirname, 'public')));
+
 app.set ('view engine', 'ejs');
 
 
 app.get ('/', function (req, res) {
-   res.render ("index");
+   res.render ("chal raha hai");
 });
 
-fs.writeFile("hey.txt ", "hey hello kaise ho", function(err) {
-    if(err) console.error(err);
-    else console.log("done");
-});
+//fs.writeFile("hey.txt ", "hey hello kaise ho", function(err) {
+    //if(err) console.error(err);
+    //else console.log("done");
+//});
 
 
 app.listen (3000, function () {
