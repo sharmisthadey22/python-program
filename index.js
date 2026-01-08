40d6bb42ffbdee065ea62592b09fef8f80d6f44f
sharmisthadey22 <sharmisthamayur@gmail.com>
2026-01-09 00:51:25 +0530

    hey

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-const express = require ('express');
-const path = require ('path');
-//const fs = require();
-const app = express ();
-
-app.use (express.json ());
-app.use (express.urlencoded ({ extended: true }));
-app.use (express.static (path.join (__dirname, 'public')));
-
-app.set ('view engine', 'ejs');
+const Path = require('path');
 
 
-app.get ('/', function (req, res) {
-   res.render ("chal raha hai");
-});
-
-//fs.writeFile("hey.txt ", "hey hello kaise ho", function(err) {
-    //if(err) console.error(err);
-    //else console.log("done");
-//});
+console.log(Path.join(__dirname, 'public'));
 
 
-app.listen (3000, function () {
 
-   console.log ("its running ");
+
+// const express = require ('express');
+// const path = require ('path');
+// //const fs = require();
+// const app = express ();
+
+// app.use (express.json ());
+// app.use (express.urlencoded ({ extended: true }));
+// app.use (express.static (path.join (__dirname, 'public')));
+
+// app.set ('view engine', 'ejs');
+
+
+// app.get ('/', function (req, res) {
+//    res.render ("chal raha hai");
+// });
+
+// //fs.writeFile("hey.txt ", "hey hello kaise ho", function(err) {
+//     //if(err) console.error(err);
+//     //else console.log("done");
+// //});
+
+
+// app.listen (3000, function () {
+
+//    console.log ("its running ");
 });
 
         
