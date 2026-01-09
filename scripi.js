e6b905d5e617f3a5b877f168cf2b9149b62959b9
sharmisthadey22 <sharmisthamayur@gmail.com>
2026-01-09 21:08:29 +0530

    rd2

--- scripi.js
+++ scripi.js
@@ -1,1 +1,12 @@
 
+
+Database -> Collection -> Document
+CODE
+````````````````````````````````````````````````````````````````````````
+
+
+CODE                      Database
+`````````````````````````````````````````````````````````````````````````
+mongoose. connect ->       database create
+model  create     ->       collection
+CREATE code       ->       document
